ef45f06b7fca1a7f5fd25733610ad71776a92dd9
Rekketstone <rekketstone@proton.me>
2024-11-26 19:04:41 -0700

    ADDED PREFIXES - ready for make install and man pages

--- config.mk
+++ config.mk
@@ -5,7 +5,11 @@
 VERSION = 0.0.1-beta
 
 # Compiler Flags & default C compiler
-CFLAGS = -Wall -g
+CFLAGS = -g -pedantic -Wall -Os
 # INCLUDES = 
 # LDFLAGS = 
 CC=cc
+
+# Install Directory
+PREFIX=/usr
+
