b5a884a4b75a24e682005ceb26c7c876020cfb1f
Rekketstone <rekketstone@proton.me>
2024-12-07 13:16:24 -0700

    repo.h changes

--- repo.h
+++ repo.h
@@ -1,17 +1,19 @@
 #ifndef REPO_H_
 #define REPO_H_
 
+#include "config.h"
+#include "package.h"
 #include <stddef.h>
 #include <unistd.h>
-#include "package.h"
-#include "config.h"
 
 // Structure to represent a single repository.
 typedef struct {
+  const char *name; // Repository name
   const char *url; // Repository URL
 } Repository;
 
 // Function declarations
-int search_all_repos(const char *package_name, const char *version, Package *pkg); // Search for a package across all repos
+int search_all_repos(const char *package_name, const char *version,
+                     Package *pkg); // Search for a package across all repos
 
-#endif // REPO_
+#endif // REPO_H_
